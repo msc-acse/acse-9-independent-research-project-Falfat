526c3733e3e887c84abf70c71d817a1f9367eee6
Falola Yusuf <fyf17@ic.ac.uk>
2019-07-26 20:09:22 +0100

    Exceptions added

--- input.py
+++ input.py
@@ -1,46 +1,56 @@
 
 def ReadFile(file):
     #open the file
-    file = open(file, "r")
-    
-    #read the lines of the file
-    f1 = file.readlines()
-    
-    for line in f1:
-        #check for a line in which starts with "radius"
-        if line.find("radius") == 0:
-            #split the line
-            line1 = line.split()
-            #store the value of radius as a float
-            radius = float(line1[2])
-        #check for a line in which starts with "length"
-        if line.find("length") == 0:
-            #split the line
-            line1 = line.split()
-            #store the value of the box length as a float
-            boxlength = float(line1[2])
-        #check for a line in which starts with "numberof circles"
-        if line.find("numberofcircles") == 0:
-            #split the line
-            line1 = line.split()
-            #store the nuber of circles as an integer
-            n = int(line1[2])
-        if line.find("orientation") == 0:
-            #split the line
-            line1 = line.split()
-            #store the orientation distribution as string
-            orientation_dist = line1[2]
-        if line.find("location") == 0:
-            #split the line
-            line1 = line.split()
-            #store the location distribution as string
-            location_dist = line1[2]
-        if line.find("shape") == 0:
-            #split the line
-            line1 = line.split()
-            #store the location distribution as string
-            fracture_shape = line1[2]
-            #print(type(fracture_shape))
+    try:
+        file = open(file, "r")
+    #if file isn't found
+    except FileNotFoundError:
+        #print error
+        print("The file can't be found in the current folder")
+    #if there is any general error    
+    except Exception as err:
+        #print that error 
+        print(err)
+    else:
+        #read the lines of the file
+        f1 = file.readlines()
+        for line in f1:
+            #check for a line in which starts with "radius"
+            if line.find("radius") == 0:
+                #split the line
+                line1 = line.split()
+                #store the value of radius as a float
+                radius = float(line1[2])
+            #check for a line in which starts with "length"
+            if line.find("length") == 0:
+                #split the line
+                line1 = line.split()
+                #store the value of the box length as a float
+                boxlength = float(line1[2])
+            #check for a line in which starts with "numberof circles"
+            if line.find("numberofcircles") == 0:
+                #split the line
+                line1 = line.split()
+                #store the nuber of circles as an integer
+                n = int(line1[2])
+            if line.find("orientation") == 0:
+                #split the line
+                line1 = line.split()
+                #store the orientation distribution as string
+                orientation_dist = line1[2]
+            if line.find("location") == 0:
+                #split the line
+                line1 = line.split()
+                #store the location distribution as string
+                location_dist = line1[2]
+            if line.find("shape") == 0:
+                #split the line
+                line1 = line.split()
+                #store the location distribution as string
+                fracture_shape = line1[2]
+                #print(type(fracture_shape))
+        #close file
+        file.close()
     return radius, boxlength, n, orientation_dist, location_dist, fracture_shape
     
 #name = "DataFile.txt"
