367b28734994873c86bcde0472de76caab5b5ea5
Falola Yusuf <fyf17@ic.ac.uk>
2019-08-19 09:41:40 +0100

    function for fracture guids added

--- DFN_Gen_Analysis_Script.py
+++ DFN_Gen_Analysis_Script.py
@@ -64,6 +64,12 @@ radius, boxlength, n, orientation_dist, location_dist, fracture_shape = input.Re
 # Uncomment the code block below                                  #
 #####                                                          ####
 
+def fractures(frac_list):
+    f = []
+    for frac in frac_list:
+        f.append(frac.fracture_GUID)
+    return f
+
 num_realisations = 300
 # list to store the percolation probability.
 # percolation probability = Np/num_of realisations
@@ -74,14 +80,16 @@ percolation_probability = []
 S_0 = []
 # initialise fractures area for all realisations 
 fractures_area = 0
-# change directory, so as to save text file in the designated folder
-os.chdir("/Users/falol/AppData/Roaming/McNeel/Rhinoceros/6.0/scripts/text_files")
-file1 = open("percolation_probability.txt", 'w')
-file2 = open("open_area.txt", 'w')
-for i in range(20,200,5):
+# define path
+#path1 = "C:/Users/falol/AppData/Roaming/McNeel/Rhinoceros/6.0/scripts/text_files/percolation_probability.txt"
+#path2 = "C:/Users/falol/AppData/Roaming/McNeel/Rhinoceros/6.0/scripts/text_files/open_area.txt"
+#file1 = open(path1, 'w')
+#file2 = open(path2, 'w')
+for i in range(5,55,5):
     # number of fractures to be generated, overites the number of fractures
     # from input file.
     n = i 
+    #fractures_area = 0
     # number of realisations that percolate
     Np = 0
     for j in range(num_realisations):
@@ -90,55 +98,57 @@ for i in range(20,200,5):
         # draws domain
         d = dom.show() 
         # set shape of farcture, so we don't have to change in file 
-        fracture_shape = 'circle' 
+        #fracture_shape = 'circle' 
         # NB: The arguments of FixedFractureGen() must be edited to suit the 
         # shape type, For instance, sides, min_angle and max_angle should be 
         # specified for a polygon
-        frac_list = DFN_Gen.FixedFractureGen() # inserts fractures and returns the GUIDs
-        dom.RemoveSurfacesOutsideOfBox(dom.length) #trims fractures outside the domain
-        dom_frac = dom.my_fractures #get the fractures in the domain
+        frac_list = DFN_Gen.FixedFractureGen(n,0,0,360,6) # inserts fractures and returns the GUIDs
+        ##dom.RemoveSurfacesOutsideOfBox(dom.length) #trims fractures outside the domain
+        ##dom_frac = dom.my_fractures #get the fractures in the domain
         #swap old guids with new ones and put new guids in old frac layers
-        new_frac_guids = Frac.new_fracture_guids(dom_frac,frac_list) 
+        ##new_frac_guids = Frac.new_fracture_guids(dom_frac,frac_list) 
         # get list of boundaries to test for percolation 
+        f = fractures(frac_list)
         boundary_list = dom.CreateBoundary(20)
         # create the intersection matrix to test for percolation
-        matrix = dom.IntersectionMatrix(boundary_list,new_frac_guids)
+        matrix = dom.IntersectionMatrix(boundary_list,f)
         # check for percolation between the two opposite boundaries specified
         # boundary_list[2] & boundary_list[4] for horizontal facing 1
         # boundary_list[3] & boundary_list[5] for horizontal facing 2
-        per1 = dom.Percolate(boundary_list[2],boundary_list[4],boundary_list,matrix,new_frac_guids)
-        per2 = dom.Percolate(boundary_list[3],boundary_list[5],boundary_list,matrix,new_frac_guids)
+        per1 = dom.Percolate(boundary_list[2],boundary_list[4],boundary_list,matrix,f)
+        #per2 = dom.Percolate(boundary_list[3],boundary_list[5],boundary_list,matrix,new_frac_guids)
         # if there is percolation
-        if per1 or per2:
+        if per1:
             # increase number of realisations that percolate
             Np+=1
         # create an instance of intersection analysis
-        ia = IntersectionAnalysis()
+        ##ia = IntersectionAnalysis()
         # determine fractures surface area for the realisation
-        frac_surf_area = ia.FracturesSurfaceArea(new_frac_guids)
+        ##frac_surf_area = ia.FracturesSurfaceArea(new_frac_guids)
         # increment fractures area for all realisations 
         # we do this because the areas will be a bit different for each 
         # realisation, due to trimming out of bounds fractures
-        fractures_area += frac_surf_area
+        ##fractures_area += frac_surf_area
         # clear screen before next iteration
         sc.doc.Objects.Clear()
     # find the total surface area of the domain
-    domain_area = 6 * boxlength**2
+    domain_area = 2400
     # determine the mean fractures surface area
-    mean_fractures_area = fractures_area/num_realisations
+    ##mean_fractures_area = fractures_area/num_realisations
     # append open area
-    S_0.append(mean_fractures_area/domain_area)
+    S_0.append((n*41.57)/domain_area)
     # append probablility
     percolation_probability.append(Np/num_realisations)
 print(S_0)
+print(percolation_probability)
     
 # write to file 
-file1.write(str(percolation_probability))
-file2.write(str(S_0))
+#file1.write(str(percolation_probability))
+#file2.write(str(S_0))
 # close file
-file1.close()
-file2.close()
-# we can then plot percolation_probability against S_0
+#file1.close()
+#file2.close()
+#we can then plot percolation_probability against S_0
 
 
 #####                                                          ####
@@ -163,7 +173,7 @@ file2.close()
 #        fracture_shape = 'circle' 
 #        # generate fractures within the range of sizes specified
 #        # here 30 fractures are being generated 
-#        frac_list = DFN_Gen.RandomFractureGen(30, 30, size_list1[i] , size_list2[i])
+#        frac_list = DFN_Gen.RandomFractureGen(20, 20, size_list1[i] , size_list2[i])
 #        dom.RemoveSurfacesOutsideOfBox(dom.length) # trims fractures outside the domain
 #        dom_frac = dom.my_fractures # get the fractures in the domain
 #        #swap old guids with new ones and put new guids in old frac layers
@@ -176,9 +186,9 @@ file2.close()
 #        # boundary_list[2] & boundary_list[4] for horizontal facing 1
 #        # boundary_list[3] & boundary_list[5] for horizontal facing 2
 #        per1 = dom.Percolate(boundary_list[2],boundary_list[4],boundary_list,matrix,new_frac_guids)
-#        per2 = dom.Percolate(boundary_list[3],boundary_list[5],boundary_list,matrix,new_frac_guids)
+#        #per2 = dom.Percolate(boundary_list[3],boundary_list[5],boundary_list,matrix,new_frac_guids)
 #        # if there is percolation
-#        if per1 or per2:
+#        if per1:
 #            # increase number of realisations that percolate
 #            Np+=1
 #            
